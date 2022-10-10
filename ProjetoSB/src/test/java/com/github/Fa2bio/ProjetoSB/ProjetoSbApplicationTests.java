d14ed74321ab2eb67be4b929e2f31ad4df3bb6fe
Fábio <fa2bio.s0@gmail.com>
2022-10-10 09:31:23 -0300

    Adicionando o metodo main

---
+++ ProjetoSB/src/test/java/com/github/Fa2bio/ProjetoSB/ProjetoSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Fa2bio.ProjetoSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
