fdd59616850cb8055d5b8271cb4c558a8089be7f
Fábio <fa2bio.s0@gmail.com>
2022-10-10 09:30:17 -0300

    Adicionando o controller da aplicação

---
+++ ProjetoSB/src/main/java/com/github/Fa2bio/ProjetoSB/controllers/ProdutoController.java
@@ -0,0 +1,71 @@
+package com.github.Fa2bio.ProjetoSB.controllers;
+
+import java.util.Optional;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.Fa2bio.ProjetoSB.models.entity.Produto;
+import com.github.Fa2bio.ProjetoSB.models.repositories.ProdutoRepository;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	//Create and Update
+	//@PostMapping
+	//@PutMapping
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Produto salvarProduto(@Valid Produto produto) {
+		produtoRepository.save(produto);
+		return produto;
+	}
+	
+	// Read
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+		return produtoRepository.findAll();
+	}
+	
+	// Read
+	@GetMapping(path="/nome/{parteNome}")
+	public Iterable<Produto> obterProdutosPorNome(@PathVariable String parteNome) {
+		return produtoRepository.findByNomeContainingIgnoreCase(parteNome);
+	}
+	
+	// Read
+	@GetMapping(path = "/pagina/{numeroPagina}/{qtdePagina}")
+	public Iterable<Produto> obterProdutosPorPagina(@PathVariable int numeroPagina, @PathVariable int qtdePagina) {
+
+		if(numeroPagina < 0) numeroPagina = 0;
+		if(qtdePagina <= 0) qtdePagina = 1;
+		if(qtdePagina>=5) qtdePagina=5;
+
+		PageRequest page = PageRequest.of(numeroPagina, qtdePagina);
+		return produtoRepository.findAll(page);
+	}
+	
+	// Read
+	@GetMapping(path="/{id}")
+	public Optional<Produto> obterProdutosId(@PathVariable int id) {
+		return produtoRepository.findById(id);
+	}
+
+	// Delete
+	@DeleteMapping(path = "/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
+	}
+
+
+}
